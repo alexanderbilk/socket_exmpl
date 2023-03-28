49fb7ef67ad370ae021118609b8492a836b4c957
Aleksandr Bilkovskii <alexanderb@nvidia.com>
2023-03-28 17:58:38 +0300

    rdmacm

--- client.c
+++ client.c
@@ -8,111 +8,154 @@
 #include <netinet/in.h>
 #include <errno.h>
 #include <unistd.h>
+#include <rdma/rdma_cma.h>
+#include <rdma/rdma_verbs.h>
 
 #include "infiniband_init.h"
 
 #define BUF_SIZE 256
+#define DEFAULT_PORT "5555"
 
+static const char *msg = "Hello from Client!";
 static void *buf;
+struct rdma_cm_id *id;
+struct ibv_mr *mr;
 
 static int
-connect_to_server(struct addrinfo *hints, char *port, int *sockfd)
+get_next_event(struct rdma_event_channel *e_channel,
+               struct rdma_cm_event **event,
+               enum rdma_cm_event_type e_type)
 {
         int rc;
-        struct addrinfo *res;
 
-        rc = getaddrinfo("localhost", "5555", hints, &res);
+        rc = rdma_get_cm_event(e_channel, event);
         if (rc) {
-                fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rc));
-                return 2;
+                fprintf(stderr, "RDMA get cm event failed with rc %d\n", rc);
+                return rc;                
         }
 
-        *sockfd = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
-        if (*sockfd == -1) {
-                fprintf(stderr, "socket: failed to create socket\n");
-                freeaddrinfo(res);
-                return errno;
+        if ((*event)->event != e_type) {
+                fprintf(stderr, "%d event was expected, %d was recieved\n", e_type, (*event)->event);
+                return -EINVAL;
         }
-
-        rc = connect(*sockfd, res->ai_addr, res->ai_addrlen);
-        if (rc == -1) {
-                fprintf(stderr, "connect: failed to connected\n");
-                freeaddrinfo(res);
-                return errno;
-        }
-
-        freeaddrinfo(res);
 
         return 0;
 }
 
 static int
-exchange_data(int sockfd, struct qp_data_s *qp_data)
+poll_cq(struct ibv_cq *cq)
 {
-        int rc;
-        struct qp_data_s *result;
+        int poll_rc;
+        struct ibv_wc wc = {0};
 
-        rc = send(sockfd, qp_data, sizeof(struct qp_data_s), 0);
-        if (rc < 0) {
-                perror("send msg");
-                return rc;
+        printf("Start polling CQ\n");
+
+        do {
+                poll_rc = ibv_poll_cq(cq, 1, &wc);
+        } while (!poll_rc);
+
+        if (poll_rc < 0) {
+                printf("Failed to poll CQ");
+                return poll_rc;
         }
 
-        sleep(1);
+        if(wc.status != IBV_WC_SUCCESS) {
+                printf("Got bad completion of status %d \n", wc.status);
+                return -EINVAL;
+        }
 
-        rc = recv(sockfd, buf, BUF_SIZE, 0);
-
-        if (rc <= 0)
-                return rc;
-
-        result = (struct qp_data_s *) buf;
-
-        printf("Recieved qp data from server\n");
-        printf("Server QP num is %d\n", result->qp_num);
-        printf("Server QP GUID is %llu\n", result->guid);
-
-        ib_setup_ud_qp(result);
+        printf("CQ is polled without errors\n");
 
         return 0;
 }
 
- int
- main(int argc, char *argv[])
- {
-        struct addrinfo hints = {0};
-        struct qp_data_s qp_data = {0};
-        int sockfd;
+int
+main(int argc, char *argv[])
+{
         int rc;
+        struct ibv_qp_init_attr qp_init_attr = {0};
+        struct rdma_event_channel *event_channel;
+        struct rdma_cm_event *event = NULL;
+        struct rdma_cm_id *cm_id;
+        struct sockaddr_in addr = { .sin_family = AF_INET, .sin_port = htons(5555), .sin_addr.s_addr = inet_addr("1.1.1.1") };
+        
 
-        hints.ai_family = AF_INET; // AF_INET или AF_INET6 если требуется
-        hints.ai_socktype = SOCK_STREAM;
-        hints.ai_flags = AI_PASSIVE;
+        event_channel = rdma_create_event_channel();
 
-        rc = connect_to_server(&hints, "5555", &sockfd);
+        rc = rdma_create_id(event_channel, &cm_id, NULL, RDMA_PS_TCP);
+        if (rc) {
+                fprintf(stderr, "RDMA create ID failed with rc %d\n", rc);
+                return rc;
+        }
+
+        rc = rdma_resolve_addr(cm_id, NULL, (struct sockaddr *)&addr, 2000);
+        if (rc) {
+                fprintf(stderr, "RDMA RESOLV ADDR failed with rc %d\n", rc);
+                return rc;                
+        }
+
+        rc = get_next_event(event_channel, &event, RDMA_CM_EVENT_ADDR_RESOLVED);
         if (rc)
                 return rc;
 
-        printf("Initialisation of QP\n");
-        rc = ib_qp_init(1, &qp_data);
-        if (rc)
-                return rc;
+        rc = rdma_resolve_route(cm_id, 2000);
+        if (rc) {
+                fprintf(stderr, "RDMA resolv route failed with rc %d\n", rc);
+                return rc;                
+        }
 
-        printf("QP num is %d\n", qp_data.qp_num);
-        printf("QP port_num is %d\n", qp_data.port_num);
-
-        buf = malloc(BUF_SIZE);
-
-        exchange_data(sockfd, &qp_data);
-
-        rc = ib_post_send("Hello from client side\n");
+        rc = get_next_event(event_channel, &event, RDMA_CM_EVENT_ROUTE_RESOLVED);
         if (rc)
                 return rc;
         
-        rc = ib_poll_cq();
+        qp_init_attr.qp_type            = IBV_QPT_RC;
+        qp_init_attr.sq_sig_all         = 1;
+     //   qp_init_attr.send_cq            = NULL;
+     //   qp_init_attr.recv_cq            = NULL;
+        qp_init_attr.cap.max_send_wr    = 1;
+        qp_init_attr.cap.max_recv_wr    = 1;
+        qp_init_attr.cap.max_send_sge   = 1;
+        qp_init_attr.cap.max_recv_sge   = 1;
+
+        rc = rdma_create_qp(cm_id, NULL, &qp_init_attr);
+        if(rc)
+        {
+                fprintf(stderr, "Failed to create QP rc %d\n", rc);
+                return rc; 
+        }
+
+        rc = rdma_connect(cm_id, NULL);
+        if (rc) {
+                fprintf(stderr, "RDMA Connect failed rc %d\n", rc);
+                return rc;                 
+        }
+
+        rc = get_next_event(event_channel, &event, RDMA_CM_EVENT_ESTABLISHED);
         if (rc)
                 return rc;
 
-        ib_destroy();
+        buf = calloc(1, BUF_SIZE);
+        if (!buf) {
+                fprintf(stderr, "Failed to alloc memmory for buffer\n");
+                return rc; 
+        }
+
+        mr = rdma_reg_msgs(cm_id, buf, BUF_SIZE);
+        if (!mr) {
+                fprintf(stderr, "Failed to create MR\n");
+                return rc; 
+        }
+
+        memcpy(buf, msg, strlen(msg));
+        rc = rdma_post_send(cm_id, NULL, buf, 70, mr, 0);
+        if(rc) {
+                fprintf(stderr, "Failed to POST recv\n");
+                return rc; 
+        }
+
+        rc = poll_cq(cm_id->send_cq);
+        if (rc)
+                return rc;
 
         return 0;
- }
+}
--- server.c
+++ server.c
@@ -9,219 +9,149 @@
 #include <sys/select.h>
 #include <errno.h>
 #include <unistd.h>
-
-#include "infiniband_init.h"
+#include <rdma/rdma_cma.h>
+#include <rdma/rdma_verbs.h>
 
 #define DEFAULT_PORT "5555"
 #define BACKLOG 10
 #define BUF_SIZE 256
 
 static void *buf;
-struct qp_data_s qp_data = {0};
+static struct ibv_mr *mr;
+static struct rdma_cm_id *listen_id, *cm_id;
+static struct rdma_event_channel *event_channel;
 
 static int
-setup_socket(struct addrinfo *hints, char *port, int *socketfd)
+get_next_event(struct rdma_event_channel *e_channel,
+               struct rdma_cm_event **event,
+               enum rdma_cm_event_type e_type)
 {
         int rc;
-        int yes = 1;
-        char server_addr[INET_ADDRSTRLEN];
-        struct addrinfo *res;
 
-        rc = getaddrinfo("localhost", port, hints, &res);
-        if (rc != 0) {
-                fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rc));
-                freeaddrinfo(res);
-                return 2;
+        rc = rdma_get_cm_event(e_channel, event);
+        if (rc) {
+                fprintf(stderr, "RDMA get cm event failed with rc %d\n", rc);
+                return rc;                
         }
 
-        inet_ntop(AF_INET, &((struct sockaddr_in *)(res->ai_addr))->sin_addr, server_addr, INET_ADDRSTRLEN);
-        printf("Server address is %s\n", server_addr);
-
-        *socketfd = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
-        if (*socketfd == -1) {
-                fprintf(stderr, "socket: failed to create socket\n");
-                freeaddrinfo(res);
-                return errno;
+        if ((*event)->event != e_type) {
+                fprintf(stderr, "%d event was expected, %d was recieved\n", e_type, (*event)->event);
+                return -EINVAL;
         }
-
-        setsockopt(*socketfd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int));
-        rc = bind(*socketfd, res->ai_addr, res->ai_addrlen);
-        if (rc == -1) {
-                fprintf(stderr, "bind: failed to bind port %s\n", port);
-                freeaddrinfo(res);
-                return errno;
-        }
-
-        rc = listen(*socketfd, BACKLOG);
-        if (rc == -1) {
-                fprintf(stderr, "listen: failed to start listening on port %s\n", port);
-                freeaddrinfo(res);
-                return errno;
-        }
-
-        printf("Listening on port %s\n", port);
-        freeaddrinfo(res);
 
         return 0;
 }
 
 static int
-accept_new_connection(int sockfd, fd_set *master, int *fdmax)
+poll_cq(struct ibv_cq *cq)
 {
-        struct sockaddr_storage remoteaddr;
-        socklen_t addrlen;
-        struct sockaddr_in *sin;
-        char addr_str[INET_ADDRSTRLEN];
-        int new_fd;
-        
-        addrlen = sizeof(remoteaddr);
-        new_fd = accept(sockfd, (struct sockaddr *) &remoteaddr, &addrlen);
-        if (new_fd == -1) {
-                fprintf(stderr, "accept: failed to accept connection on port %s\n", DEFAULT_PORT);
-                return errno;
+        int poll_rc;
+        struct ibv_wc wc = {0};
+
+        printf("Start polling CQ\n");
+
+        do {
+                poll_rc = ibv_poll_cq(cq, 1, &wc);
+        } while (!poll_rc);
+
+        if (poll_rc < 0) {
+                printf("Failed to poll CQ");
+                return poll_rc;
         }
 
-        sin = (struct sockaddr_in *) &remoteaddr;
-        inet_ntop(AF_INET, &sin->sin_addr, addr_str, INET_ADDRSTRLEN);
-        printf("Accepted new connection from %s Created new fd %d \n", addr_str, new_fd);
+        if(wc.status != IBV_WC_SUCCESS) {
+                printf("Got bad completion of status %d \n", wc.status);
+                return -EINVAL;
+        }
 
-        FD_SET(new_fd, master);
-        if (new_fd >= *fdmax)
-                *fdmax = new_fd + 1;
+        printf("CQ is polled without errors\n");
 
         return 0;
 }
 
-static inline void
-close_connection(int fd, fd_set *master)
-{
-        printf("Closing connection on fd %d", fd);
-        FD_CLR(fd, master);
-        close(fd);
-}
-
-static int
-exchange_data(int fd, fd_set *master)
+int
+main(int argc, char *argv[])
 {
         int rc;
-        struct qp_data_s *result;
+        struct rdma_cm_event *event = NULL;
+        struct ibv_qp_init_attr qp_init_attr = {0};
+        struct sockaddr_in addr = { .sin_family = AF_INET, .sin_port = htons(5555), .sin_addr.s_addr = inet_addr("1.1.1.1") };
 
-        rc = recv(fd, buf, BUF_SIZE, 0);
+        event_channel = rdma_create_event_channel();
 
-        if (rc == 0)
-                return 0;
-
-        if (rc < 0) {
-                close_connection(fd, master);
+        rc = rdma_create_id(event_channel, &listen_id, NULL, RDMA_PS_TCP);
+        if (rc) {
+                fprintf(stderr, "RDMA create ID failed with rc %d\n", rc);
                 return rc;
         }
 
-        result = (struct qp_data_s *) buf;
-        
-        printf("New data has been recieved from connection %d\n", fd);
-        printf("Client QP num is %d\n", result->qp_num);
-        printf("Client QP GUID is %llu\n", result->guid);
-
-        rc = ib_setup_ud_qp(result);
-
-        if (rc)
+        rc = rdma_bind_addr(listen_id, (struct sockaddr *)&addr);
+        if (rc) {
+                fprintf(stderr, "RDMA BIND ADDR failed with rc %d\n", rc);
                 return rc;
-        
-        printf("Sending data qp_data back to client\n");
-        rc = send(fd, &qp_data, sizeof(struct qp_data_s), 0);
-        if (rc < 0)
-                return rc;
-
-        return 0;
-}
-
-static int
-proccesing(int sockfd)
-{
-        fd_set master;
-        fd_set active;
-        int fdmax;
-        int new_fd;
-        int rc;
-        int i;     
-
-        FD_ZERO(&master);
-        FD_ZERO(&active);
-        FD_SET(sockfd, &master);
-        fdmax = sockfd + 1;
-
-        while (1) {
-                active = master;
-                rc = select(fdmax, &active, NULL, NULL, NULL);
-                if (rc == -1) {
-                        fprintf(stderr, "select: failed to get list of readable fd\n");
-                        return errno;
-                }
-
-                for (i = sockfd; i < fdmax; i++) {
-                        if (FD_ISSET(i, &active)) {
-                                if (i == sockfd) {
-                                        rc = accept_new_connection(sockfd, &master, &fdmax);
-                                        if (rc)
-                                                perror("accept new connection");
-                                } else {
-                                        rc = exchange_data(i, &master);
-                                        if (rc)
-                                                perror("recieve data");
-
-                                        goto connection_done;
-                                }
-                        }
-                }
         }
 
-connection_done:
+        rc = rdma_listen(listen_id, 0);
+        if (rc) {
+                fprintf(stderr, "RDMA LISTEN failed with rc %d\n", rc);
+                return rc;
+        }
+        
+        rc = get_next_event(event_channel, &event, RDMA_CM_EVENT_CONNECT_REQUEST);
+        if(rc)
+                return rc;
+
+        qp_init_attr.qp_type            = IBV_QPT_RC;
+        qp_init_attr.sq_sig_all         = 1;
+     //   qp_init_attr.send_cq            = NULL;
+     //   qp_init_attr.recv_cq            = NULL;
+        qp_init_attr.cap.max_send_wr    = 1;
+        qp_init_attr.cap.max_recv_wr    = 1;
+        qp_init_attr.cap.max_send_sge   = 1;
+        qp_init_attr.cap.max_recv_sge   = 1;
+
+        cm_id = event->id;
+
+        rc = rdma_create_qp(cm_id, NULL, &qp_init_attr);
+        if (rc) {
+                fprintf(stderr, "Failed to create QP rc %d\n", rc);
+                return rc; 
+        }
+
+        rc = rdma_accept(cm_id, NULL);
+        if (rc) {
+                fprintf(stderr, "Failed to accept connection rc %d\n", rc);
+                return rc; 
+        }
+
+        rc = get_next_event(event_channel, &event, RDMA_CM_EVENT_ESTABLISHED);
+        if(rc)
+                return rc;
+
+        rdma_ack_cm_event(event);
+        buf = calloc(1, BUF_SIZE);
+        if (!buf) {
+                fprintf(stderr, "Failed to alloc memmory for buffer\n");
+                return rc; 
+        }
+
+        mr = rdma_reg_msgs(cm_id, buf, BUF_SIZE);
+        if (!mr) {
+                fprintf(stderr, "Failed to create MR\n");
+                return rc; 
+        }
+
+        rc = rdma_post_recv(cm_id, NULL, buf, 70, mr);
+        if(rc) {
+                fprintf(stderr, "Failed to POST recv\n");
+                return rc; 
+        }
+
+        rc = poll_cq(cm_id->recv_cq);
+        if (rc)
+                return rc;
+
+        printf("Recieved msg : %s\n", buf);
+
         return 0;
 }
-
- int
- main(int argc, char *argv[])
- {
-        int sockfd;
-        int rc;
-        struct addrinfo hints = {0};
-
-        hints.ai_family = AF_INET; // AF_INET или AF_INET6 если требуется
-        hints.ai_socktype = SOCK_STREAM;
-        hints.ai_flags = AI_PASSIVE;
-        
-        rc = setup_socket(&hints, DEFAULT_PORT, &sockfd);
-        if (rc)
-                return rc;
-
-        printf("Initialisation of QP\n");
-        rc = ib_qp_init(0, &qp_data);
-        if (rc)
-                return rc;
-        
-        printf("QP num is %d\n", qp_data.qp_num);
-        printf("QP port_num is %d\n", qp_data.port_num);
-
-        buf = malloc(BUF_SIZE);
-
-        rc = proccesing(sockfd);
-        if (rc)
-                return rc;
-
-        printf("Data exchange completed\n");
-        
-        rc = ib_post_recieve();
-        if (rc)
-                return rc;
-        
-        rc = ib_poll_cq();
-        if (rc)
-                return rc;
-
-        ib_print_buffer_and_flush();
-
-
-        ib_destroy();
-
-        return 0;
- }
